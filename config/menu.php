bf9a1830f6082d60fcf3e54084004dc6f5d3d9a2
Ivan Mercedes <ivanmercede@gmail.com>
2023-04-29 21:19:41 -0400

    Update navbar with dynamic menu and listing config

---
+++ config/menu.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    [
+        'text' => 'Home',
+        'route' => 'home.index',
+    ],
+    [
+        'text' => 'Developers',
+        'route' => 'developers.index',
+    ],
+    [
+        'text' => 'Jobs',
+        'route' => 'jobs.index',
+    ],
+    [
+        'text' => 'About',
+        'route' => 'about.index',
+    ],
+];
