aa261ca7f00c631ac11a9470f1c01ac9497c1c21
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2023-05-01 00:50:48 -0400

    Remove unused route

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -61,12 +61,6 @@
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
-                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                {{ __('API Tokens') }}
-                            </x-dropdown-link>
-                        @endif
-
                         <div class="border-t border-gray-200"></div>
 
                         <!-- Authentication -->
@@ -144,12 +138,6 @@
                                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                                     {{ __('Profile') }}
                                 </x-responsive-nav-link>
-
-                                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                    <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
-                                        {{ __('API Tokens') }}
-                                    </x-responsive-nav-link>
-                                @endif
 
                                 <!-- Authentication -->
                                 <form method="POST" action="{{ route('logout') }}" x-data>
