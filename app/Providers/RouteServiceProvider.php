b919063d995ec9f7170970c0a2a120f9ebb247e3
Fermin <fperdomo@truelogic.io>
2023-05-08 10:31:14 -0400

    Fix issue with the tests

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/dashboard';
 
+    protected $namespace = '';
+
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
