02838033624d402b20f87d2b221e9e121630ee1d
Fermin <fperdomo@truelogic.io>
2023-05-09 20:04:44 -0400

    story 14 - created form to add new job

--- app/Http/Livewire/CreateJobForm.php
+++ app/Http/Livewire/CreateJobForm.php
@@ -3,23 +3,49 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
-use DateTimeInterface;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class CreateJobForm extends Component
 {
+    /** @var array<int, array<string, string>> */
+    private array $remotePolicies = [
+        ['value' => 'Remote', 'label' => 'Remote'],
+        ['value' => 'Onsite', 'label' => 'Onsite'],
+        ['value' => 'Hybrid', 'label' => 'Hybrid'],
+    ];
+
+    /** @var array<int, array<string, string>> */
+    private array $jobTypes = [
+        ['value' => 'Full-time', 'label' => 'Full-time'],
+        ['value' => 'Part-time', 'label' => 'Part-time'],
+        ['value' => 'Contract', 'label' => 'Contract'],
+        ['value' => 'Freelance', 'label' => 'Freelance'],
+        ['value' => 'Internship', 'label' => 'Internship'],
+    ];
+
+    /** @var array<int, array<string, string>> */
+    private array $jobLevels = [
+        ['value' => 'Junior', 'label' => 'Junior'],
+        ['value' => 'Mid', 'label' => 'Mid'],
+        ['value' => 'Senior', 'label' => 'Senior'],
+        ['value' => 'Lead', 'label' => 'Lead'],
+    ];
+
     public User $user;
 
-    /** @var array{title: string, description: string, budget: string, category_id: string, photo_url: string, starts_at: ?DateTimeInterface, ends_at: ?DateTimeInterface}  */
+    /** @var array{title: string, company_name: string, salary: number, experience_level: string, job_type: string, job_level: string, remote_policy: string, description: string, photo_url: string, photo_path: string }  */
     public array $state = [
         'title' => '',
+        'company_name' => '',
+        'remote_policy' => 'Remote',
+        'job_type' => '',
+        'experience_level' => '',
+        'salary' => 0,
         'description' => '',
-        'budget' => '',
-        'category_id' => '',
+        'job_level' => '',
         'photo_url' => '',
-        'starts_at' => null,
-        'ends_at' => null,
+        'photo_path' => '',
     ];
 
     /** @var array<string, string> */
@@ -28,17 +54,30 @@ class CreateJobForm extends Component
     public function mount(User $user): void
     {
         $this->user = $user;
-        $this->state['starts_at'] = now();
-        $this->state['ends_at'] = now();
+        $this->state['photo_url'] = $this->defaultPhotoUrl();
     }
 
     public function render(): View
     {
-        return view('livewire.create-job-form');
+        return view('livewire.create-job-form', [
+            'remotePolicies' => $this->remotePolicies,
+            'jobTypes' => $this->jobTypes,
+            'jobLevels' => $this->jobLevels,
+        ]);
+    }
+
+    public function createJob(): void
+    {
+
     }
 
     public function savedAction(): void
     {
         $this->emit('saved');
+    }
+
+    private function defaultPhotoUrl(): string
+    {
+        return 'https://ui-avatars.com/api/?name=company_name&color=7F9CF5&background=EBF4FF';
     }
 }
--- resources/views/livewire/create-job-form.blade.php
+++ resources/views/livewire/create-job-form.blade.php
@@ -11,38 +11,38 @@
         <x-slot name="form">
             <!-- Job Photo -->
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
-                <!-- Job Photo File Input -->
+                <!-- Profile Photo File Input -->
                 <input type="file" class="hidden"
                        wire:model="photo"
                        x-ref="photo"
                        x-on:change="
-                                photoName = $refs.photo.files[0].name;
-                                const reader = new FileReader();
-                                reader.onload = (e) => {
-                                    photoPreview = e.target.result;
-                                };
-                                reader.readAsDataURL($refs.photo.files[0]);
-                        " />
+                                    photoName = $refs.photo.files[0].name;
+                                    const reader = new FileReader();
+                                    reader.onload = (e) => {
+                                        photoPreview = e.target.result;
+                                    };
+                                    reader.readAsDataURL($refs.photo.files[0]);
+                            " />
 
                 <x-label for="photo" value="{{ __('Photo') }}" />
 
-                <!-- Current job Photo -->
+                <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $state['photo_url'] }}" alt="{{ $user->name }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
-                <!-- New Job Photo Preview -->
+                <!-- New Profile Photo Preview -->
                 <div class="mt-2" x-show="photoPreview" style="display: none;">
-                                <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                                      x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
-                                </span>
+                    <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
+                          x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                    </span>
                 </div>
 
                 <x-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
                     {{ __('Select A New Photo') }}
                 </x-secondary-button>
 
-                @if ($user->profile_photo_path)
+                @if ($state['photo_path'])
                     <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
                         {{ __('Remove Photo') }}
                     </x-secondary-button>
@@ -51,11 +51,56 @@
                 <x-input-error for="photo" class="mt-2" />
             </div>
 
-            <!-- Name -->
+            <!-- Title -->
             <div class="col-span-6 sm:col-span-4">
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="state.name" autocomplete="name" />
-                <x-input-error for="name" class="mt-2" />
+                <x-label for="title" value="{{ __('Title') }}" />
+                <x-input id="title" type="text" class="mt-1 block w-full" wire:model.defer="state.title" autocomplete="title" />
+                <x-input-error for="title" class="mt-2" />
+            </div>
+
+            <!-- Company Name -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="company_name" value="{{ __('Company Name') }}" />
+                <x-input id="company_name" type="text" class="mt-1 block w-full" wire:model.defer="state.company_name" autocomplete="company_name" />
+                <x-input-error for="company_name" class="mt-2" />
+            </div>
+
+            <!-- Remote policy -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="remote_policy" value="{{ __('Remote policy') }}" />
+                <x-select id="remote_policy" class="mt-1 block w-full" :options="$remotePolicies" wire:model.defer="state.remote_policy" autocomplete="remote_policy">
+                </x-select>
+                <x-input-error for="remote_policy" class="mt-2" />
+            </div>
+
+            <!-- Job Type -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="job_type" value="{{ __('Job Type') }}" />
+                <x-select id="job_type" class="mt-1 block w-full" :options="$jobTypes" wire:model.defer="state.job_type" autocomplete="job_type">
+                </x-select>
+                <x-input-error for="job_type" class="mt-2" />
+            </div>
+
+            <!-- Job Level -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="job_level" value="{{ __('Job Level') }}" />
+                <x-select id="job_level" class="mt-1 block w-full" :options="$jobLevels" wire:model.defer="state.job_level" autocomplete="job_level">
+                </x-select>
+                <x-input-error for="job_level" class="mt-2" />
+            </div>
+
+            <!-- Salary -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="salary" value="{{ __('Salary') }}" />
+                <x-input id="salary" type="text" class="mt-1 block w-full" wire:model.defer="state.salary" autocomplete="salary" />
+                <x-input-error for="salary" class="mt-2" />
+            </div>
+
+            <!-- Description -->
+            <div class="col-span-6 sm:col-span-4">
+                <x-label for="description" value="{{ __('Description') }}" />
+                <x-textarea id="description" type="text" class="mt-1 block w-full" wire:model.defer="state.description" autocomplete="description" />
+                <x-input-error for="description" class="mt-2" />
             </div>
 
         </x-slot>
