35d7b6bb213c983f46a0dc17ce2a3a6b4d0b55ce
Elminson De Oleo Baez <edeoleo@gmail.com>
2023-08-29 22:53:47 -0400

    adding jobs and users seeder

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\Traits\HasPhoto;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
     use HasPhoto;
+	use HasFactory;
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,23 +4,29 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\JobSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
 
-        // \App\Models\User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 */
+	public function run()
+	: void
+	{
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([
-            RoleSeeder::class,
-        ]);
-    }
+		// \App\Models\User::factory(10)->create();
+
+		// \App\Models\User::factory()->create([
+		//     'name' => 'Test User',
+		//     'email' => 'test@example.com',
+		// ]);
+		$this->call([
+						JobSeeder::class,
+						UserSeeder::class,
+						// RoleSeeder::class,
+					]);
+	}
 }
